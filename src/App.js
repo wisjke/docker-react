ec2c1013cb311d1caeb647723e1fca955fc5140e
wisjke <143843322+wisjke@users.noreply.github.com>
2024-10-23 19:34:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          sorry nigers
         </p>
         <a
           className="App-link"
